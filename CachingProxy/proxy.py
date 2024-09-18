36f0dc66a5a421342674219da10224ec5ffc12f1
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-18 13:28:20 -0300

    Starting project Caching Proxy with a simple proxy

---
+++ CachingProxy/proxy.py
@@ -0,0 +1,27 @@
+import argparse
+from flask import Flask, request, Response
+import requests
+
+parser = argparse.ArgumentParser(prog="Caching Proxy")
+parser.add_argument("-p", "--port", type=int)
+parser.add_argument("-o", "--origin", type=str)
+
+args = parser.parse_args()
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path=''):
+    method = request.method
+    data = request.get_data()
+    headers = {key: value for key, value in request.headers if key != 'Host'}
+
+    dest_url = f"{args.origin}/{path}"
+    response = requests.request(method, dest_url, headers=headers, data=data)
+
+    return Response(response.content, response.status_code)
+
+
+app.run(debug=True, port=args.port)

5b6aa438b1d4d4be0c8aadf13a71d80bfbcffc06
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-20 15:20:16 -0300

    Using typer and pyproject to create command line tool

--- CachingProxy/proxy.py
+++ CachingProxy/proxy.py
@@ -1,28 +1,33 @@
-import argparse
 from flask import Flask, request, Response
+from typing_extensions import Annotated
 import requests
-
-parser = argparse.ArgumentParser(prog="Caching Proxy")
-parser.add_argument("-p", "--port", type=int)
-parser.add_argument("-o", "--origin", type=str)
-
-args = parser.parse_args()
-
-app = Flask(__name__)
+import typer
 
 
-@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def proxy(path=''):
-    method = request.method
-    data = request.get_data()
-    headers = {key: value for key, value in request.headers if key != 'Host'}
+def caching_proxy(
+    port: Annotated[int, typer.Option(help="Port used to run the proxy")] = 3000,
+    origin: Annotated[str, typer.Option(help="link to the website that will be targeted by the proxy")] = "http://dummyjson.com"
+):
+    app = Flask(__name__)
 
-    dest_url = f"{args.origin}/{path}"
-    response = requests.request(method, dest_url, headers=headers, data=data)
-    headers = [(k, v) for k, v in response.headers.items()
-               if k not in ('Content-Length', 'Content-Encoding', 'Transfer-Encoding', 'Connection')]
-    return Response(response.content, response.status_code, headers=headers)
+    @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    def proxy(path=''):
+        method = request.method
+        data = request.get_data()
+        headers = {key: value for key, value in request.headers if key != 'Host'}
+
+        dest_url = f"{origin}/{path}"
+        response = requests.request(method, dest_url, headers=headers, data=data)
+        headers = [(k, v) for k, v in response.headers.items()
+                   if k not in ('Content-Length', 'Content-Encoding', 'Transfer-Encoding', 'Connection')]
+        return Response(response.content, response.status_code, headers=headers)
+
+    app.run(debug=True, port=port)
 
 
-app.run(debug=True, port=args.port)
+app = typer.Typer()
+app.command()(caching_proxy)
+
+if __name__ == "__main__":
+    app()
--- CachingProxy/readme.md
+++ CachingProxy/readme.md
@@ -1,3 +1,9 @@
 # Caching Proxy
 
 This project involves building a simple command-line interface (CLI) tool that starts a caching proxy server. The server forwards requests to an origin server and caches the responses. If the same request is made again, the proxy returns the cached response instead of forwarding the request.
+
+> pipx ensurepath
+
+restart the terminal
+
+> pipx install .

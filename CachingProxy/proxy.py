492506f5986f7c3913cf805d461defcfc80f2189
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-21 10:12:56 -0300

    Inclunding cache to the proxy

--- CachingProxy/proxy.py
+++ CachingProxy/proxy.py
@@ -3,6 +3,8 @@ from typing_extensions import Annotated
 import requests
 import typer
 
+cache = {}
+
 
 def caching_proxy(
     port: Annotated[int, typer.Option(help="Port used to run the proxy")] = 3000,
@@ -18,10 +20,21 @@ def caching_proxy(
         headers = {key: value for key, value in request.headers if key != 'Host'}
 
         dest_url = f"{origin}/{path}"
-        response = requests.request(method, dest_url, headers=headers, data=data)
-        headers = [(k, v) for k, v in response.headers.items()
-                   if k not in ('Content-Length', 'Content-Encoding', 'Transfer-Encoding', 'Connection')]
-        return Response(response.content, response.status_code, headers=headers)
+
+        if dest_url not in cache.keys():
+            response = requests.request(method, dest_url, headers=headers, data=data)
+            headers = [(k, v) for k, v in response.headers.items()
+                       if k not in ('Content-Length', 'Content-Encoding', 'Transfer-Encoding', 'Connection', 'X-Cache')]
+            content, status_code = response.content, response.status_code
+
+            cache[dest_url] = (content, status_code, headers)
+            headers.append(('X-Cache', 'MISS'))
+
+        else:
+            content, status_code, headers = cache[dest_url]
+            headers.append(('X-Cache', 'HIT'))
+
+        return Response(content, status_code, headers=headers)
 
     app.run(debug=True, port=port)
 

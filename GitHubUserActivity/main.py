f149124aa956e94398b3b49bc27c3391e651aa52
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-30 20:54:48 -0300

    Implement install through PIPX with pyproject.toml

--- GitHubUserActivity/main.py
+++ GitHubUserActivity/main.py
@@ -4,6 +4,16 @@ import json
 from typing_extensions import Annotated
 
 
+messages = {
+    "PushEvent": "Pushed to",
+    "IssueCommentEvent": "Issue Commented in",
+    "IssuesEvent": "Opened a new issue in",
+    "PullRequestEvent": "New pull request",
+    "WatchEvent": "A new user is watching",
+    "ForkEvent": "A new fork was created to"
+}
+
+
 def github_activity(
         username: Annotated[str, typer.Argument(help="Github username")]
 ):
@@ -12,8 +22,7 @@ def github_activity(
     content = json.loads(response.content)
 
     for row in content:
-        print(row.keys())
-        print(row["type"])
+        print(f"{messages[row['type']] if row['type'] in messages.keys() else row['type']} {row['repo']['name']}")
 
 
 app = typer.Typer()
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@ Project ideas from Roadmap.sh to take you from beginner to advanced in Backend
 
 1. [Task Tracker](https://roadmap.sh/projects/task-tracker)
 2. [Caching Proxy](https://roadmap.sh/projects/caching-server)
+3. [GitHubUserActivity](..%2FCachingProxy%2Fpyproject.toml)
